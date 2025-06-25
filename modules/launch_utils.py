269000e9fda8888ce1849e8dfc39ee9ca2607446
yiohngkljaf <1391048988@QQ.com>
2025-06-25 18:43:15 +0800

    support uv

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -150,7 +150,7 @@ def run_pip(command, desc=None, live=default_command_live):
         return
 
     index_url_line = f' --index-url {index_url}' if index_url != '' else ''
-    return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
+    return run(f'uv pip {command}, desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
 
 
 def check_run_python(code: str) -> bool:
